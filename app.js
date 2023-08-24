4fe3d8ef249a6633d381b6ee21de1410cb3f5850
Hayden Hargreaves <hhargr836@west-mec.org>
2023-08-24 14:04:02 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Imports
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+
+// Static files & images
+app.use(express.static("./public/images"));
+
+// Set views & view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// Load pages from views
+app.get("/*", (req, res) => {
+    if (req.url === "/") {
+        res.render('home');
+    } else {
+        res.render(req.url.slice(1));
+    }
+});
+
+// Listen for connections
+app.listen(PORT, () => {
+    console.log(`Running on port: ${PORT}`);
+});
