63e875f0bf9733a3eb5d25faf124d3b9c9716baf
Hayden Hargreaves <hhargr836@west-mec.org>
2023-09-05 14:03:03 -0700

    Added pages, displays 10 games per page.

--- app.js
+++ app.js
@@ -17,17 +17,24 @@ const data = require("./data/NintendoGames.json");
 data.sort((a, b) => a.title.localeCompare(b.title));
 
 // Load login page: load all games
-app.get("/", (req, res) => {    
-    res.status(200).render("index", { data });
+app.get("/", (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    const count = 10;
+    const pageCount = (Math.floor(data.length / 10) + 1);
+    const results = data.slice(((page - 1) * count), ((page - 1) * count) + count);
+    res.status(200).render("index", { data: results, pageCount: pageCount || 0 });       
 });
 
 // Query the json file for games with the matching title and render resulting cards
 app.get("/search", (req, res) => {
-    const query = req.query.titles.toLowerCase();
-    const valid = data.filter(game => game.title.toLowerCase().includes(query));
-    res.status(200).render("index", { data: valid });       
+    const titles = req.query.titles.toLowerCase();
+    const page = parseInt(req.query.page);
+    const count = 10;
+    const valid = data.filter(game => game.title.toLowerCase().includes(titles));
+    const pageCount = (Math.floor(valid.length / 10) + 1);
+    const results = valid.slice(((page - 1) * count), ((page - 1) * count) + count);
+    res.status(200).render("index", { data: results, pageCount: pageCount || 0 });       
 });
-
 
 // Launch server
 app.listen(3000, () => console.log("Server is active at localhost:3000/"));
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "media",
   content: ["views/*.{html,ejs,js}", "views/partials/*.{html,ejs,js}"],
   theme: {
     extend: {},
