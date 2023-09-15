0b26f8a565edec97208bab2a9d9dbc158e8d73a0
Hayden Hargreaves <104033825+Azpect3120@users.noreply.github.com>
2023-09-15 14:10:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -37,4 +37,5 @@ app.get("/search", (req, res) => {
 });
 
 // Launch server
-app.listen(3000, () => console.log("Server is active at localhost:3000/"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server is active on port " + PORT));
